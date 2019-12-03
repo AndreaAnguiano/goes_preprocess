2d1c12836a0ec687350154cc9d845698048241a0
Andrea Isabel Anguiano García <andrea_anguiano@ciencias.unam.mx>
2019-12-02 21:29:04 -0600

    post_process path fixed

--- scripts/main.py
+++ scripts/main.py
@@ -10,7 +10,7 @@ fs = s3fs.S3FileSystem(anon=True)
 # List contents of GOES-16 bucket.
 fs.ls('s3://noaa-goes16/')
 # define path of data, product and date
-data_path = '../data/'
+data_path = '../../data/'
 goes = 'noaa-goes16'
 product = 'ABI-L2-CMIPF'
 
@@ -28,7 +28,7 @@ filteredFiles = np.array([[f for f in files if 'M6C01' in f][0], [f for f in fil
 # create the download paths
 today_path = str(datetime.now().year) + '-' + '{0:02d}'.format(datetime.now().month) + '-'+'{0:02d}'.format(datetime.now().day)
 abs_path = os.path.abspath(os.path.join(data_path, today_path))
-pre_process_path = os.path.join(abs_path, 'pre_process')
+post_process_path = os.path.join(abs_path, 'post_process')
 
 if not os.path.exists(abs_path):
     os.mkdir(abs_path)
@@ -39,9 +39,9 @@ else:
 
 [fs.get(filteredFiles[i], abs_path + '/' + filteredFiles[i].split('/')[-1]) for i in range(0, len(filteredFiles))]
 
-if not os.path.exists(pre_process_path):
+if not os.path.exists(post_process_path):
     os.mkdir(pre_process_path)
-    [os.mkdir(os.path.join(pre_process_path, 'band' + str(indx))) for indx in range(0, len(filteredFiles))]
+    [os.mkdir(os.path.join(post_process_path, 'band' + str(indx))) for indx in range(0, len(filteredFiles))]
     print('Daily directory created')
 else:
     print('Daily directory already exists')
@@ -49,6 +49,6 @@ else:
 latbox = [18.2, 31]
 lonbox = [-98, -83]
 
-pre_processGOES(abs_path, latbox, lonbox, pre_process_path, datetime.now(), bands)
+pre_processGOES(abs_path, latbox, lonbox, post_process_path, datetime.now(), bands)
 
 [os.remove(f) for f in os.listdir(abs_path) if f != 'post_process']
